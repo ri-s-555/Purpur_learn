79305c196a5e5bd7dcf04153422c0c00363007b1
ri_s <henviar39@gmail.com>
2025-02-13 01:40:37 +0300

    создала карточку

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 console.log("Hello, Webpack!");
 
 import './style.css';
+
+import '../css/style.css' 
+
+import {creatCard} from '/card.js' 
+const cards = [
+    {
+        color: 'green', 
+        price: 0, 
+        name: '' ,
+        review: 0
+    }
+]
+cards. forEach()
+
+
+const elem = creatCard ("card-color_blu" )
